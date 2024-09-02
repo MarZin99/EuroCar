a0d8c5d2d2da85d62199d0ee04f1e1b1021cdecd
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-09-02 22:19:20 +0200

    Started adding below md hamburger

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -2,17 +2,19 @@
 import Link from "next/link";
 import Image from "next/image";
 import thunder from "../../public/svg/thunder.svg";
+import { useState } from "react";
 
 export default function TopNav() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const pages = [
     { name: "Galeria zdjęć", path: "/galery" },
     { name: "FaQ", path: "/faq" },
   ];
 
   return (
-    <nav className="h-20 p-4">
-      <div className="flex justify-between h-12 mx-36">
-        <div className="flex  w-72 items-center">
+    <nav className="h-20 p-4 bg-red-500">
+      <div className="flex justify-between h-12 md:mx-36">
+        <div className="flex w-72 items-center">
           <div>
             <Image src={thunder} alt="Thunder logo" height={36} />
           </div>
@@ -21,18 +23,19 @@ export default function TopNav() {
             <div>Spot</div>
           </div>
         </div>
-        <div className="w-36 flex justify-evenly items-center">
+        <div className="hidden w-36 md:flex justify-evenly items-center">
           {pages.map((item, key) => (
             <Link key={key} href={item.path}>
               {item.name}
             </Link>
           ))}
         </div>
-        <div className="w-72 flex justify-end">
+        <div className="hidden w-72 md:flex justify-end">
           <button className="bg-blue-600 rounded-md px-5 py-2 text-white">
             Zadzwoń do nas
           </button>
         </div>
+        <div className="md:hidden bg-red-300">burger</div>
       </div>
     </nav>
   );
