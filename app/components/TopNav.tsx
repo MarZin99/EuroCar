a3819e3017f4ad563dca29d8114644a9ec257e97
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-09-02 22:00:21 +0200

    Added png files, created and added svg background lines, deleted unused svg

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Link from "next/link";
 import Image from "next/image";
-import thunder from "../../public/thunder.svg";
+import thunder from "../../public/svg/thunder.svg";
 
 export default function TopNav() {
   const pages = [
